6aac5268b6ae1870bdef96ada60b036393819286
parzuag3 <164633063+parzuag3@users.noreply.github.com>
2026-02-05 07:15:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace structassignmentpedroarzuaga
+{
+    struct WizardCurrency
+    {
+        public int galleons;
+        public int sickles;
+        public int knuts;
+
+        public void WizardCurrencly(int newGalleon, int newSickle, int newknut)
+        {
+            galleons = newGalleon;
+            sickles = newSickle;
+            knuts = newknut;
+            WizardCurrency myMoney = new WizardCurrency();
+            myMoney.galleons = 5;
+            myMoney.sickles = 12;
+            myMoney.knuts = 20;
+        }
+        void Add(int Galleons, int Sickles, int Knuts)
+        {
+            Galleons = Galleons + galleons;
+            Sickles = Sickles + sickles;
+            Knuts = Knuts + knuts;
+            while (sickles > 29)
+                while (knuts > 17)
+                {
+                    Knuts -= 18;
+                    Sickles++;
+                }
+            {
+                Sickles -= 30;
+                Galleons++;
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
---
+++ practice 8/practice 8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace practice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 10;
+            Console.WriteLine(first);
+            int second = 4;
+                Console.WriteLine(second);
+            int third = 5;
+                Console.WriteLine(third);
+            Console.WriteLine("x = 10 - 4 / 5. What is the sum value of x?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == 10)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else if (answer <= 10)
+            {
+                Console.WriteLine("Sorry, wrong answer. The answer is:");
+                    }
+            else if (answer >= 10)
+            {
+                Console.WriteLine("Sorry, wrong answer. The answer is:");
+            }
+
+            int sum = first - second / third;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+            int sum1 = (first - second) / third;
+            Console.WriteLine(sum1);
+        }
+    }
+}
